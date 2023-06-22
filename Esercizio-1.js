4b304aa45a05b8508534dcae23f035eaa8d03569
Giovanni198914 <136268951+Giovanni198914@users.noreply.github.com>
2023-06-23 00:00:51 +0200

    Add files via upload

---
+++ Esercizio-1.js
@@ -0,0 +1,9 @@
+//Cos'e un algoritmo?
+//E' un insieme di step necessari alla risoluzioni di un problema. Attraverso vari passaggi (input) determino il risultato finale (output)
+
+//Cos'e una variabile?
+//Sono dei contenitori per dati che vengono elaborati parzialmente. La variabile va dichiarata in quanto bisogna assegnarle un valore.
+
+//Undefined e null sono la stessa cosa?
+//Undefined rappresenta un valore al momento non esiste. Alla variabile non e' stato assegnato alcun valore.
+//Null rappresenta un valore vuoto in quanto la variabile non contiene nulla.
